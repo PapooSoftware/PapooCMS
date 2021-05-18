0f1f151213dd6717806393ee04a1f8fde4735522
Christoph Zimmer <cz@papoo.de>
2021-05-18 13:15:14 +0200

    Fixed updating primary key in update routine

--- update/update_latest.sql
+++ update/update_latest.sql
@@ -4,6 +4,7 @@ ALTER TABLE `XXX_papoo_freiemodule_daten` ADD COLUMN `freiemodule_lang_id` INT(1
 UPDATE `XXX_papoo_freiemodule_daten` _module
 LEFT JOIN `XXX_papoo_name_language` _lang ON _lang.lang_short LIKE _module.freiemodule_lang
 SET _module.freiemodule_lang_id = COALESCE(_lang.lang_id, 1); ##b_dump##
+ALTER TABLE `XXX_papoo_freiemodule_daten` MODIFY `freiemodule_id` INT NOT NULL; ##b_dump##
 ALTER TABLE `XXX_papoo_freiemodule_daten` DROP PRIMARY KEY; ##b_dump##
 ALTER TABLE `XXX_papoo_freiemodule_daten` ADD PRIMARY KEY (`freiemodule_id`, `freiemodule_lang_id`); ##b_dump##
 ALTER TABLE `XXX_papoo_freiemodule_daten` MODIFY `freiemodule_id` INT NOT NULL AUTO_INCREMENT; ##b_dump##
