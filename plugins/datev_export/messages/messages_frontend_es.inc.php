ba8ba025067252a5eb0cf2bf2242c34d31792a3d
Carsten Euwens <ce@papoo.de>
2021-05-19 13:48:51 +0200

    DEL - Datev Export Plugin deleted - it does require the deprecated shop which ist not on board any more...

--- plugins/datev_export/messages/messages_frontend_es.inc.php
+++
@@ -1,6 +0,0 @@
-<?php 
-$this->content->template['message']['plugin']['test']['name'] = "Plugin \"Test"; 
-$this->content->template['message']['plugin']['test']['text'] = "<h3>En</h3>la definición de los archivos de idioma se pueden utilizar etiquetas HTML, pero hay que tener en cuenta lo siguiente<ul><li>Los saltos de línea se convierten en &lt;br /&gt;s en la salida</li><li>Los archivos deben guardarse en el formato \"UTF-8 (sin BOM)\"</li></ul>"; 
-$this->content->template['message']['plugin']['test']['test_text'] = "¡Hola, mundo!"; 
-
- ?>
